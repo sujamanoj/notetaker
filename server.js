6f45567ac06ec81f64ffbc2ac10604395fcb11fb
sujamanoj <suja@masstv.app>
2022-07-18 14:28:33 -0700

    route update

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./Develop/routes/apiRoutes/noteRoutes');
-const htmlRoutes = require('./Develop/routes/htmlRoutes/index');
+const apiRoutes = require("./routes/apiRoutes/noteRoutes");
+const htmlRoutes = require("./routes/htmlRoutes/index");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(express.static(__dirname + '/Develop/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
